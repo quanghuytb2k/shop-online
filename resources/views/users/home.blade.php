b73bc5b7129bd626e7c298ff0a4579b15d95d4ec
viett9 <vietthep289@gmail.com>
2024-12-05 22:20:07 +0700

    create home user 3

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -37,7 +37,7 @@
                 </div>    
             </div>
             <div class="container mx-auto mt-4">
-                <h2 class="bg-orange-500 text-white px-4 py-2">Hot Deal</h2>
+                <h2 class="bg-orange-500 font-bold text-white px-4 py-2">Hot Deal</h2>
                 <div class="flex flex-wrap md:flex-nowrap space-y-4 md:space-y-0 md:space-x-4 mt-2">
                     <div class="w-full md:w-1/4 bg-white p-4 border border-gray-200 md:space-y-0 md:space-x-4">
                         <img alt="Hot deal image 1" class="h-32 w-full object-cover mb-2" height="150" src="https://storage.googleapis.com/a1aa/image/dCzDq6j3ZmLsGxlfzbbxsyewgsQ2z08VWaZt96aPtE1jeqrnA.jpg" width="200"/>
@@ -52,9 +52,85 @@
                         <img alt="Hot deal image 4" class="h-32 w-full object-cover mb-2" height="150" src="https://storage.googleapis.com/a1aa/image/tpzX0HZ5IaZRARODmmetOleWc34l0WuxCT28ppLKgRGgeqrnA.jpg" width="200"/>
                     </div>
                 </div>
+                <div class="mt-8">
+                    <h2 class="text-lg font-bold text-white bg-orange-500 p-2">Sản phẩm cùng loại cùng tầm giá</h2>
+                    <div class="p-4 rounded-b-lg grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                        <div class="relative hover:border-red-500">
+                            <img alt="Brown striped polo shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01rKgbX91spmSJV0QsD_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã C0191 Giá 830K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Kín Cổ Thời Trang Nam</p>
+                            <p class="text-lg text-red-500">830.000 đ</p>
+                            <p class="line-through">830.000 đ</p>
+                            <p>Đã bán 104</p>
+                        </div>
+                        <div class="relative">
+                            <img alt="White polo shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01dvKGCX1spmSFuMMYW_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã C0291 Giá 890K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Hàng Mùa Hè Kín Cổ Thời Trang Nam Chất Liệu</p>
+                            <p class="text-lg text-red-500">890.000 đ</p>
+                            <p class="line-through">890.000 đ</p>
+                            <p>Đã bán 104</p>
+                        </div>
+                        <div class="relative">
+                            <img alt="Black polo shirt with logo" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01dvKGCX1spmSFuMMYW_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã B9146 Giá 1000K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Kín Cổ Thời Trang Nam</p>
+                            <p class="text-lg text-red-500">890.000 đ</p>
+                            <p class="line-through">1.000.000 đ</p>
+                            <p>Đã bán 100</p>
+                        </div>
+                        <div class="relative">
+                            <img alt="Beige polo shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01Od1N171spmSFuquOT_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã B9177 Giá 1020K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Thời Trang Nam Chất Liệu</p>
+                            <p class="text-lg text-red-500">890.000 đ</p>
+                            <p class="line-through">1.020.000 đ</p>
+                            <p>Đã bán 82</p>
+                        </div>
+                        <div class="relative">
+                            <img alt="Black button-up shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01jI5Yf91spmSFunLv5_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã B8841 Giá 940K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Kín Cổ Thời Trang Nam Chất</p>
+                            <p class="text-lg text-red-500">940.000 đ</p>
+                            <p class="line-through">940.000 đ</p>
+                            <p>Đã bán 96</p>
+                        </div>
+                    </div>
+                    <div class="p-4 rounded-b-lg grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                        <div class="relative hover:border-red-500">
+                            <img alt="Brown striped polo shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01rKgbX91spmSJV0QsD_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã C0191 Giá 830K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Kín Cổ Thời Trang Nam</p>
+                            <p class="text-lg text-red-500">830.000 đ</p>
+                            <p class="line-through">830.000 đ</p>
+                            <p>Đã bán 104</p>
+                        </div>
+                        <div class="detail_product">
+                            <img alt="White polo shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01dvKGCX1spmSFuMMYW_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã C0291 Giá 890K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Hàng Mùa Hè Kín Cổ Thời Trang Nam Chất Liệu</p>
+                            <p class="text-lg text-red-500">890.000 đ</p>
+                            <p class="line-through">890.000 đ</p>
+                            <p>Đã bán 104</p>
+                        </div>
+                        <div class="detail_product">
+                            <img alt="Black polo shirt with logo" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01dvKGCX1spmSFuMMYW_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã B9146 Giá 1000K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Kín Cổ Thời Trang Nam</p>
+                            <p class="text-lg text-red-500">890.000 đ</p>
+                            <p class="line-through">1.000.000 đ</p>
+                            <p>Đã bán 100</p>
+                        </div>
+                        <div class="detail_product">
+                            <img alt="Beige polo shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01Od1N171spmSFuquOT_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã B9177 Giá 1020K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Thời Trang Nam Chất Liệu</p>
+                            <p class="text-lg text-red-500">890.000 đ</p>
+                            <p class="line-through">1.020.000 đ</p>
+                            <p>Đã bán 82</p>
+                        </div>
+                        <div class="detail_product">
+                            <img alt="Black button-up shirt" class="w-full h-auto" height="300" src="https://cbu01.alicdn.com/img/ibank/O1CN01jI5Yf91spmSFunLv5_!!2206365235816-0-cib.jpg" width="200"/>
+                            <p class="text-sm">(Mới) Mã B8841 Giá 940K: Áo Sơ Mi Nam Sutder Big Size Ngoại Cỡ Ngắn Tay Hàng Mùa Hè Kín Cổ Thời Trang Nam Chất</p>
+                            <p class="text-lg text-red-500">940.000 đ</p>
+                            <p class="line-through">940.000 đ</p>
+                            <p>Đã bán 96</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
 @endsection
 
